55d56ac1b39ef9c6a07f2c71a9cd0924d8d1ea28
Abhay Amin <pixoloproductions@gmail.com>
2015-08-28 16:20:15 +0530

    base

---
+++ application/models/Activetime_model.php
@@ -0,0 +1,11 @@
+<?php 
+ defined('BASEPATH') OR exit('No direct script access allowed'); 
+ 
+ class Activetime_model extends PIXOLO_Model 
+ { 
+	 public $_table = 'activetime';  
+ 
+ 	 //Write functions here 
+ } 
+ 
+ ?>
---
+++ application/models/Categorie_model.php
@@ -0,0 +1,11 @@
+<?php 
+ defined('BASEPATH') OR exit('No direct script access allowed'); 
+ 
+ class Categorie_model extends PIXOLO_Model 
+ { 
+
+ 
+ 	 //Write functions here 
+ } 
+ 
+ ?>
